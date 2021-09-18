d50adc3a97262231847fedc2542e7b23f128d7bd
arya2008 <74598441+arya2008@users.noreply.github.com>
2021-09-18 16:59:46 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,13 @@
+class box{
+    constructor(x,y,width,height){
+        this.width=width
+        this.height=height
+        this.body=Bodies.rectangle(x,y,width,height,{isStatic:true})
+        World.add(world,this.body)
+    }
+    display(){
+        rectMode(CENTER)
+        fill("red")
+        rect(this.body.position.x,this.body.position.y,this.width,this.height)
+    }
+}
